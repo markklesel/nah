0abdb49baf2be380a600437ed7914be0b1038263
Owen Rumney <owen.rumney@aquasec.com>
2021-09-03 08:33:02 +0100

    make metadata hack

--- cmd/tfsec-json-export/main.go
+++ cmd/tfsec-json-export/main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/aquasecurity/tfsec/pkg/rule"
 	"io/ioutil"
 	"os"
+	"sort"
 	"strings"
-
-	"github.com/aquasecurity/tfsec/pkg/rule"
 
 	_ "github.com/aquasecurity/tfsec/internal/app/tfsec/rules"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 )
 
+type reference struct {
+	Title string `json:"title"`
+	Url   string `json:"url"`
+}
+
 type custom struct {
 	Severity     string              `json:"severity"`
 	Impact       string              `json:"possibleImpact"`
-	Urls         []string            `json:"urls"`
+	References   []reference            `json:"references"`
 	Apis         []string            `json:"apis"`
 	ExternalRefs map[string][]string `json:"refs"`
 }
@@ -30,48 +35,127 @@ type metadata struct {
 	Custom      custom `json:"custom"`
 }
 
-func newMetadata(r rule.Rule) metadata {
+type fileContent struct {
+	Provider string
+	Checks   []rule.Rule
+}
 
-	return metadata{
-		Id:          "AQUA_ID_GOES_HERE",
+var providers map[string]string = map[string]string{
+	"AWS":          "AWS",
+	"AZURE":        "AZU",
+	"CLOUDSTACK":   "CSK",
+	"DIGITALOCEAN": "DIG",
+	"GENERAL":      "GEN",
+	"GITHUB":       "GIT",
+	"GOOGLE":       "GCP",
+	"KUBERNETES":   "K8S",
+	"OPENSTACK":    "OSK",
+	"ORACLE":       "OCI",
+}
+
+func newMetadata(id string, r rule.Rule) metadata {
+
+	m := metadata{
+		Id:          id,
 		ApiVersion:  2,
 		Version:     1,
 		Title:       r.Documentation.Summary,
 		Description: strings.TrimSpace(r.Documentation.Explanation),
 		Custom: custom{
-			Severity: string(r.DefaultSeverity),
-			Impact:   r.Documentation.Impact,
-			Urls:     r.Documentation.Links,
-			Apis:     []string{},
+			Severity:   string(r.DefaultSeverity),
+			Impact:     r.Documentation.Impact,
+			Apis:       []string{},
 			ExternalRefs: map[string][]string{
-				"tfsec": []string{r.ID()},
-				"cspm":  []string{},
 				"cfsec": []string{},
+				"cspm":  []string{},
+				"tfsec": []string{r.ID()},
 			},
 		},
 	}
 
+	for _, link := range r.Documentation.Links {
+		if strings.Contains(link, "terraform") {
+			continue
+		}
+		m.Custom.References = append(m.Custom.References, reference{Title: "title", Url: link})
+	}
+
+
+	if strings.ToUpper(string(r.Provider)) == "AWS" {
+		m.Custom.ExternalRefs["cfsec"] = append(m.Custom.ExternalRefs["cfsec"], r.ID())
+	}
+	return m
 }
 
 func main() {
 
-	for _, rule := range scanner.GetRegisteredRules() {
+	providersIndex := make(map[string]map[string]string)
 
-		path := fmt.Sprintf("../cloud-metadata/%s/%s/%s/", rule.Provider, rule.Service, rule.ShortCode)
+	for _, c := range getSortedfileContents() {
 
-		if err := os.MkdirAll(path, 0777); err != nil {
-			panic(err)
+		providerIndex := make(map[string]string)
+
+		for i, r := range c.Checks {
+
+			provider := providers[strings.ToUpper(c.Provider)]
+			id := fmt.Sprintf("AVD-%s-%04d", provider, i+1)
+
+			fmt.Printf("Writing file for %s\n", id)
+			path := fmt.Sprintf("%s/%s/%s/", c.Provider, r.Service, r.ShortCode)
+			providerIndex[id] = fmt.Sprintf("%smetadata.json", path)
+			if err := os.MkdirAll(path, 0777); err != nil {
+				panic(err)
+			}
+
+			metadata := newMetadata(id, r)
+
+			body, err := json.MarshalIndent(metadata, "", "  ")
+			if err != nil {
+				panic(err)
+			}
+
+			ioutil.WriteFile(fmt.Sprintf("../cloud-metadata/%s/metadata.json", path), body, 0777)
 		}
 
-		metadata := newMetadata(rule)
-
-		body, err := json.MarshalIndent(metadata, "", "  ")
+		body, err := json.MarshalIndent(providerIndex, "", "  ")
 		if err != nil {
 			panic(err)
 		}
+		ioutil.WriteFile(fmt.Sprintf("../cloud-metadata/%s/index.json", c.Provider), body, 0777)
+		providersIndex[c.Provider] = providerIndex
+	}
+	body, err := json.MarshalIndent(providersIndex, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	ioutil.WriteFile("../cloud-metadata/index.json", body, 0777)
 
-		ioutil.WriteFile(fmt.Sprintf("%s/metadata.json", path), body, 0777)
+}
 
+func getSortedfileContents() []*fileContent {
+	rules := scanner.GetRegisteredRules()
+
+	checkMap := make(map[string][]rule.Rule)
+
+	for _, r := range rules {
+		provider := string(r.Provider)
+		checkMap[provider] = append(checkMap[provider], r)
 	}
 
+	var fileContents []*fileContent
+	for provider := range checkMap {
+		checks := checkMap[provider]
+		sortChecks(checks)
+		fileContents = append(fileContents, &fileContent{
+			Provider: provider,
+			Checks:   checks,
+		})
+	}
+	return fileContents
+}
+
+func sortChecks(checks []rule.Rule) {
+	sort.Slice(checks, func(i, j int) bool {
+		return checks[i].ID() < checks[j].ID()
+	})
 }
